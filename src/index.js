493fb5063ca173d27ea5d4f0980faecb73178b6b
yusivacova <sivacovayulia@gmail.com>
2023-10-22 00:09:31 +0300

    feat: add burger menu

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import '@babel/polyfill';
 import './index.html';
 import './styles/index.scss';
 
+import { createBurgerMenu } from './script/burger';
+
 window.onload = function () {
+  createBurgerMenu();
 };
